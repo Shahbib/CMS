849dfd6ec375eed425e13e375986f4eaabfcd9f7
Shahbib Ayman Shabib <shabibayman89@gmail.com>
2024-05-11 23:05:27 +0600

    Add files via upload

---
+++ cms/src/main/java/com/shabib/cms/CmsApplication.java
@@ -0,0 +1,16 @@
+package com.shabib.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CmsApplication.class, args);
+    }
+
+
+
+}
